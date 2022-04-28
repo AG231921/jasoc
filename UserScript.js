5dd9225d4840ff72ed84c9ad7db7c5c679372559
AG231921 <56095475+AG231921@users.noreply.github.com>
2022-04-28 13:47:14 -0400

    Add files via upload

---
+++ UserScript.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+function js() {
+window.open("https://www.google.com");
+}
