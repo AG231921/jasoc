732fc640f3668fbd5a5290c67186c208bb2bc73f
AG231921 <56095475+AG231921@users.noreply.github.com>
2022-04-28 13:54:03 -0400

    Update UserScript.js

--- UserScript.js
+++ UserScript.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://github.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
+// @downloadURL  https://raw.githubusercontent.com/AG231921/jasoc/main/UserScript.js
+// @updateURL    https://raw.githubusercontent.com/AG231921/jasoc/main/UserScript.js
 // ==/UserScript==
 
 function js() {
